d6b95fa11d7d5b63a90a209c8849eafe157ac36f
Joakim Holm <joakimholm@protonmail.com>
2022-08-01 22:46:37 +0200

    Set docker to be started manually

--- nixos/modules/work.nix
+++ nixos/modules/work.nix
@@ -3,7 +3,10 @@
 {
   config = lib.mkIf config.personal.work.enable {
     # Jsfix config
-    virtualisation.docker.enable = true;
+    virtualisation.docker = {
+      enable = true;
+      enableOnBoot = false;
+    };
     users.users.jo1gi.extraGroups = [ "docker" ];
     environment.systemPackages = with pkgs; [
       docker-compose
