912d73b87e5db1ce1a0a037a69f4d482c4fe3e0e
Joakim Holm <mail@joakimholm.xyz>
2023-06-28 10:35:42 +0200

    Update flake inputs

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     unstable.url = "github:NixOS/nixpkgs";
     nur = {
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     base16.url = "github:alukardbf/base16-nix";
