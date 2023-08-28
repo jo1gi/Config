8063d3a7fea0ece879dae2522756b6692919ed9b
Joakim Holm <mail@joakimholm.xyz>
2023-08-28 13:44:37 +0200

    Update nim config

--- home/modules/programming/nim.nix
+++ home/modules/programming/nim.nix
@@ -3,14 +3,23 @@
 {
   config = lib.mkIf config.jo1gi.programming.nim.enable {
 
-    home.packages = with pkgs; [
+    home.packages = with pkgs.unstable; [
       nim
       nimlsp
     ];
 
+    home.file."${config.home.homeDirectory}/.config/nimble/nimble.ini".text = ''
+      nimbleDir = r"~/.local/share/nimble"
+    '';
+
+    home.sessionPath = [
+      "$HOME/.local/share/nimble/bin"
+    ];
+
     programs.neovim = {
       extraLuaConfig = ''
-        require('jo1gi.helpers.setup_lsp')("nimls")
+        -- require('jo1gi.helpers.setup_lsp')("nim_langserver")
+        require('jo1gi.helpers.setup_lsp')("nimlsp")
       '';
       plugins = with pkgs.vimPlugins; [
         nim-vim
