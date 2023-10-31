50fdd21527e45b6b41275b1cb55c19e4e8e26340
Joakim Holm <mail@joakimholm.xyz>
2023-10-31 12:47:57 +0100

    Remove custom ocaml

--- home/packages/ocaml.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, fetchurl, ... }:
-
-ocaml.overrideAttrs {
-  version = "5.0.0";
-  src = fetchurl {
-    url = "http://caml.inria.fr/pub/distrib/ocaml-5.0/ocaml-5.0.0.tar.xz";
-    sha256 = "sha256-yxfwpTTdSz/sk9ARsL4bpcYIfaAzz3iehaNLlkHsxl8=";
-  };
-}
