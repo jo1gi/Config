5747f822f22b2f363ce78b41b4e33e10af022b2c
Joakim Holm <mail@joakimholm.xyz>
2023-11-13 13:49:52 +0100

    Optimize find-projects script

--- home/modules/programs/find-projects/find-projects.zsh
+++ home/modules/programs/find-projects/find-projects.zsh
@@ -1,5 +1,5 @@
 function projects() {
-    project=$(find "${FIND_PROJECTS_DIR}" -name '.git' | grep -Po "(?<=(${FIND_PROJECTS_DIR}/)).+(?=(/.git))" | sk)
+    project=$(find "${FIND_PROJECTS_DIR}" -maxdepth 4 -mindepth 2 -name '.git' | grep -Po "(?<=(${FIND_PROJECTS_DIR}/)).+(?=(/.git))" | sk)
     workspace_num=$(swaymsg -t get_workspaces | jq '. | map(select(.focused)) | .[0].num')
     swaymsg "rename workspace to \"$workspace_num: $project\""
     cd "${FIND_PROJECTS_DIR}/$project"
