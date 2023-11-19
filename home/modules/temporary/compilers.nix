b48be9a15fd38d9f7fff50d5f1400d2e47defaaf
Joakim Holm <mail@joakimholm.xyz>
2023-11-19 21:31:56 +0100

    Add vim dolphin plugin

--- home/modules/temporary/compilers.nix
+++ home/modules/temporary/compilers.nix
@@ -1,7 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   jo1gi.programming = {
     ocaml.enable = true;
+  };
+
+  programs.neovim = {
+    plugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        pname = "dolphin.vim";
+        version = "1";
+        src = pkgs.fetchFromGitLab {
+          owner = "jo1gi";
+          repo = "dolphin-vim";
+          rev = "1bddf3c798cbb425f0a288c1a3640e06bea2fccc";
+          sha256 = "sha256-28fPWSYOHyBLiwVkGyYoslpWnqrBFozaSnhFNQ8NG9o=";
+        };
+      })
+    ];
   };
 }
