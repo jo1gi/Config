6588ae967f76d8913b80c2766e6a750d6859fb69
Joakim Holm <mail@joakimholm.xyz>
2024-02-21 09:24:31 +0100

    Update neovim icons

--- dotfiles/.config/neovim/lua/jo1gi/lsp.lua
+++ dotfiles/.config/neovim/lua/jo1gi/lsp.lua
@@ -37,7 +37,7 @@ require('lspkind').init({
     },
 })
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = "󰌵 ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
