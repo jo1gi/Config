9887ebb18c3340c7ac21480a4f0d540504aaf4c1
Joakim Holm <mail@joakimholm.xyz>
2024-05-17 18:25:06 +0200

    Set mpv yt-dlp quality to <=1080

--- home/modules/programs/mpv.nix
+++ home/modules/programs/mpv.nix
@@ -6,7 +6,7 @@
       vo = "gpu";
       hwdec = "auto-safe";
       profile = "gpu-hq";
-      ytdl-format = "best[height<=720]";
+      ytdl-format = "best[height<=1080]";
       osc = "no";
     };
     scripts = with pkgs.mpvScripts; [
